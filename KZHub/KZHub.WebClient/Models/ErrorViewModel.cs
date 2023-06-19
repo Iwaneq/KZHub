8815316f0dcbbd81099bbd0dba35a0a4766d03d9
Iwaneq <iwanskikuba@wp.pl>
2023-06-19 20:24:49 +0200

    Add MVC project

---
+++ KZHub/KZHub.WebClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KZHub.WebClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
