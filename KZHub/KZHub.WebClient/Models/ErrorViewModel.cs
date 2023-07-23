f3d2a5db9ef27434acd0de6f73dbb47db6abc6d0
Iwaneq <iwanskikuba@wp.pl>
2023-07-23 17:57:30 +0200

    Remove project

--- KZHub/KZHub.WebClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KZHub.WebClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
