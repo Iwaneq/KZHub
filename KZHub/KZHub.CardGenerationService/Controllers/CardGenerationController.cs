1c1f04020f42a476d35215478e67c9aad6fd0377
Iwaneq <iwanskikuba@wp.pl>
2023-07-24 17:01:26 +0100

    Create a CardGenerationService
    * Add new project
    
    * Configure K8S

---
+++ KZHub/KZHub.CardGenerationService/Controllers/CardGenerationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KZHub.CardGenerationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardGenerationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TestConnection()
+        {
+            Console.WriteLine("--> Testing connection");
+
+            return Ok();
+        }
+    }
+}
