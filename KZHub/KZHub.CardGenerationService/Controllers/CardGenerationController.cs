6b6d06c47766ddb6d9f206268f147cba0a52b9ce
Iwaneq <iwanskikuba@wp.pl>
2023-07-25 10:27:18 +0100

    Add generate card functionallity

--- KZHub/KZHub.CardGenerationService/Controllers/CardGenerationController.cs
+++ KZHub/KZHub.CardGenerationService/Controllers/CardGenerationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KZHub.CardGenerationService.DTOs.Card;
+using KZHub.CardGenerationService.Services.CardProcessing.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KZHub.CardGenerationService.Controllers
@@ -7,6 +8,31 @@ namespace KZHub.CardGenerationService.Controllers
     [ApiController]
     public class CardGenerationController : ControllerBase
     {
+        private readonly ICardGenerator _generator;
+
+        public CardGenerationController(ICardGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        [HttpPost]
+        public IActionResult GenerateCard(CreateCardDTO createCard)
+        {
+            try
+            {
+                var card = _generator.GenerateCard(createCard);
+
+                if (card == null) throw new Exception("Card was null!");
+
+                return Ok(card);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Exception occured while generating card: {ex.Message}");
+                return BadRequest();
+            }
+        }
+
         [HttpGet]
         public IActionResult TestConnection()
         {
---
+++ KZHub/KZHub.CardGenerationService/DependencyInjection/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using KZHub.CardGenerationService.Services.CardProcessing;
+using KZHub.CardGenerationService.Services.CardProcessing.Interfaces;
+
+namespace KZHub.CardGenerationService.DependencyInjection
+{
+    public static class ConfigureServices
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICardGenerator, CardGenerator>();
+        }
+    }
+}
